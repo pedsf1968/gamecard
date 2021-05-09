25db95819ce484c071f8cea6ec88683acff7d493
PEDSF <pe.dsf@free.fr>
2021-05-09 10:19:55 +0200

    Add graphic interface

--- src/main/java/com/pedsf/cardgame/controller/GameController.java
+++ src/main/java/com/pedsf/cardgame/controller/GameController.java
@@ -5,6 +5,7 @@ import com.pedsf.cardgame.model.Deck;
 import com.pedsf.cardgame.model.Player;
 import com.pedsf.cardgame.model.PlayingCard;
 import com.pedsf.cardgame.view.CommandLineView;
+import com.pedsf.cardgame.view.GameViewable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,33 +19,33 @@ public class GameController {
    }
 
    Deck deck;
-   CommandLineView commandLineView;
+   GameViewable gameViewable;
    GameEvaluator gameEvaluator;
    List<Player> players;
    Player winner;
    GameState gameState;
 
-   public GameController(Deck deck, CommandLineView commandLineView, GameEvaluator gameEvaluator) {
+   public GameController(Deck deck, GameViewable gameViewable, GameEvaluator gameEvaluator) {
       super();
       this.deck = deck;
-      this.commandLineView = commandLineView;
+      this.gameViewable = gameViewable;
       this.gameEvaluator = gameEvaluator;
       this.players = new ArrayList<>();
       this.gameState = GameState.ADDING_PLAYERS;
-      commandLineView.setController(this);
+      gameViewable.setController(this);
    }
 
    public void run() {
       while (gameState == GameState.ADDING_PLAYERS) {
-         commandLineView.promptForPlayerName();
+         gameViewable.promptForPlayerName();
       }
 
       switch (gameState) {
          case CARDS_DEALT:
-            commandLineView.promptForFlip();
+            gameViewable.promptForFlip();
             break;
          case WINNER_REVEALED:
-            commandLineView.promptForNewGame();
+            gameViewable.promptForNewGame();
             break;
          default:
       }
@@ -53,7 +54,7 @@ public class GameController {
    public void addPlayer(String playerName) {
       if (gameState == GameState.ADDING_PLAYERS) {
          players.add(new Player(playerName));
-         commandLineView.showPlayerName(players.size(), playerName);
+         gameViewable.showPlayerName(players.size(), playerName);
       }
    }
 
@@ -63,7 +64,7 @@ public class GameController {
          int playerIndex = 1;
          for (Player player : players) {
             player.addCardToHand(deck.removeTopCard());
-            commandLineView.showFaceDownCardForPlayer(playerIndex++, player.getName());
+            gameViewable.showFaceDownCardForPlayer(playerIndex++, player.getName());
          }
          gameState = GameState.CARDS_DEALT;
       }
@@ -75,7 +76,7 @@ public class GameController {
       for (Player player : players) {
          PlayingCard pc = player.getCard(0);
          pc.flip();
-         commandLineView.showCardForPlayer(playerIndex++, player.getName(),
+         gameViewable.showCardForPlayer(playerIndex++, player.getName(),
                pc.getRank().toString(), pc.getSuit().toString());
       }
 
@@ -91,7 +92,7 @@ public class GameController {
    }
 
    void displayWinner() {
-      commandLineView.showWinner(winner.getName());
+      gameViewable.showWinner(winner.getName());
    }
 
    void rebuildDeck() {
--- src/main/java/com/pedsf/cardgame/games/Games.java
+++ src/main/java/com/pedsf/cardgame/games/Games.java
@@ -3,13 +3,17 @@ package com.pedsf.cardgame.games;
 import com.pedsf.cardgame.controller.GameController;
 import com.pedsf.cardgame.model.Deck;
 import com.pedsf.cardgame.view.CommandLineView;
+import com.pedsf.cardgame.view.GameSwingView;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Games {
    public static void main(String[] args) {
+      GameSwingView gsw = new GameSwingView();
+      gsw.createAndShowGUI();
+
       // GameController gc = new GameController(new Deck(), new CommandLineView(), new HighCardGameEvaluator());
-      GameController gc = new GameController(new Deck(), new CommandLineView(), new LowCardGameEvaluator());
+      GameController gc = new GameController(new Deck(), gsw, new LowCardGameEvaluator());
 
       gc.run();
    }
